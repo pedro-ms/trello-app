9470f13e9a676e50694c57874d586858967afa08
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 12:32:12 -0300

    Fix params route

--- app/routes/stories/edit.js
+++ app/routes/stories/edit.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function () {
-    return this.store.createRecord('story', params.story_id);
+  model(params) {
+    return this.store.findRecord('story', params.story_id);
   },
 
   actions: {
