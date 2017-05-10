d36bf7c069de53a780efa793d13935046cf4959a
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 11:56:55 -0300

    Create route stories/new

--- app/routes/stories/new.js
+++ app/routes/stories/new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return this.store.createRecord('story');
+  },
+
+  actions: {
+    saveStory(newStory) {
+      newStory.save().then(() => this.transitionTo('stories'));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  }
 });
