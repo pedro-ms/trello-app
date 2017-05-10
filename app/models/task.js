1bd6157ad72db2fc9cbe89bef433d2c26538d510
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 14:20:35 -0300

    Add validation model

--- app/models/task.js
+++ app/models/task.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  story: DS.belongsTo('story')
+  story: DS.belongsTo('story'),
+
+  isValidName: Ember.computed.notEmpty('name'),
+  isValidStory: Ember.computed.notEmpty('story'),
+  isValid: Ember.computed.and('isValidName', 'isValidStory')
 });
