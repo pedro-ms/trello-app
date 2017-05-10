bf6099c38d05333e84d766ce51de4956ca1726b2
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 13:56:48 -0300

    Create route tasks/edit

--- app/routes/tasks/edit.js
+++ app/routes/tasks/edit.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('task', params.task_id);
+  },
+
+  actions: {
+    saveTask(editTask) {
+      let storyId = this.controller.get('storyId');
+      let story = this.get('store').peekRecord('story', storyId);
+
+      newTask.set('story', story);
+      newTask.save().then(() => this.transitionTo('tasks'));
+    }
+  }
 });
