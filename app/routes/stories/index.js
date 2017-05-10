c7d53baede5fe07bbc1930df2bf412b39f998c05
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 17:16:20 -0300

    Add button new

--- app/routes/stories/index.js
+++ app/routes/stories/index.js
@@ -4,12 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('story');
   },
-  
+
   actions: {
     deleteStory(story) {
       let confirmation = confirm('Are you sure?');
-
+      let hasTask = this.controller.get('model.story.tasks.length()');
+      console.log(hasTask)
       if (confirmation) {
+
         story.destroyRecord();
       }
     }
