901ac42f1f242047cbaeb4ee58f3b112c06579db
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 15:35:30 -0300

    Add action deleteTask

--- app/routes/stories/view.js
+++ app/routes/stories/view.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('story', params.story_id);
+  },
+
+  actions: {
+    deleteTask(task) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        task.destroyRecord();
+      }
+    }
   }
 });
