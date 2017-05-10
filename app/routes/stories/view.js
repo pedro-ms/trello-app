3f132ae266691e958fe5acb41d8d7ec199af6489
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 14:55:45 -0300

    Add include tasks in findRecord

--- app/routes/stories/view.js
+++ app/routes/stories/view.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('story', params.story_id);
+    return this.store.findRecord('story', params.story_id, {include: 'tasks'});
   }
 });
