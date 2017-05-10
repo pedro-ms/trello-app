27cd24ce4cf0cbc1c86725ad50646fac95459e3e
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 13:06:16 -0300

    Add model validation

--- app/models/story.js
+++ app/models/story.js
@@ -3,5 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   dueDate: DS.attr('date'),
-  tasks: DS.hasMany('task')
+  tasks: DS.hasMany('task'),
+
+  isValidTitle: Ember.computed.notEmpty('title'),
+  isValidDueDate: Ember.computed.notEmpty('dueDate'),
+  isValid: Ember.computed.and('isValidTitle', 'isValidDueDate')
 });
