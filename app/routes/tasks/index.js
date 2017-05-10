a194766a32df4602033dffe60e54f8878d3ca019
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 13:49:56 -0300

    Add action deleteTask

--- app/routes/tasks/index.js
+++ app/routes/tasks/index.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('task');
+  },
+
+  actions: {
+    deleteTask(task) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        task.destroyRecord();
+      }
+    }
   }
 });
