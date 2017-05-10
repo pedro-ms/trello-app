139f7e84f865a82e7f8638ca3a91d86381783b41
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 15:31:20 -0300

    Create route stories/new-task

--- app/routes/stories/new-task.js
+++ app/routes/stories/new-task.js
@@ -4,20 +4,19 @@ export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
       task: this.store.createRecord('task'),
-      story: this.store.findRecord('story', params.story_id)
+      story: this.store.findRecord('story', params.story_id )
     })
   },
 
   actions: {
     saveTask(newTask) {
-      let storyId = this.controller.get('model.story.id');
-      let story = this.get('store').peekRecord('story', storyId);
+      let story = this.controller.get('model.story');
 
       newTask.set('story', story);
       story.get('tasks').pushObject(newTask);
       newTask.save().then(function () {
         story.save();
-      }).then(() => this.transitionTo('tasks'));
+      }).then(() => this.transitionTo('stories'));
     },
 
     willTransition() {
