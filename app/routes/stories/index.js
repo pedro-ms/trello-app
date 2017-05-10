49f93e543a8020906ac109e5633b1ef878ad8eb1
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 12:35:00 -0300

    Create action deleteStory

--- app/routes/stories/index.js
+++ app/routes/stories/index.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('story');
+  },
+  
+  actions: {
+    deleteStory(story) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        story.destroyRecord();
+      }
+    }
   }
 });
