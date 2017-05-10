05022770cfc04b2758a3f10d03caed22f42026fd
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-10 15:12:28 -0300

    Change name route stories/tasks to stories/new-task

--- app/router.js
+++ app/router.js
@@ -12,7 +12,7 @@ Router.map(function() {
     this.route('new');
     this.route('edit', { path: '/:story_id/edit' });
     this.route('view', { path: '/:story_id/view' });
-    this.route('tasks');
+    this.route('new-task', { path: '/:story_id/new-task' });
   });
   this.route('tasks', function() {
     this.route('index');
